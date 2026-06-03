d81f84ef39c8d98381275130be9fa213f878d127
Vinayak Sinha <vinayaksinha@zoho.com>
2026-06-03 20:40:27 +0530

    Added react router, created YearReview and LoginPage tsx files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,7 @@ import LoginButton from "./components/LoginButton";
 function App() {
   const [accessToken, setAccessToken] = useState("");
 
-  return (
-    <>
-      <LoginButton />
-    </>
-  );
+  return <>{accessToken !== "" ? <h1>Logged In</h1> : <LoginButton />}</>;
 }
 
 export default App;
