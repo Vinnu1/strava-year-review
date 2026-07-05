484ace5fe5b2b6c8c51080b106cab22f527afea4
Vinayak Sinha <vinayaksinha@zoho.com>
2026-07-05 19:30:15 +0530

    Wrote pandas code to get top sports by distance

--- server/app/services/data_analysis.py
+++ server/app/services/data_analysis.py
@@ -5,4 +5,14 @@ with open('../../tests/dump.json', 'r') as file:
     data = json.load(file)
 
 df = pd.DataFrame(data)
-print(df.iloc[0])
+# print(df.iloc[0])
+
+# walk_km = df.loc[df["type"] == "Walk", "distance"].sum()/1000
+# run_km = df.loc[df["type"] == "Run", "distance"].sum()/1000
+# print(walk_km, run_km)
+
+# type_km = (df.groupby("type")["distance"].sum()/1000).sort_values(ascending=False)
+# print(type_km)
+
+weight_train = df.loc[df["type"] == "WeightTraining"]
+print(weight_train.iloc[0])
