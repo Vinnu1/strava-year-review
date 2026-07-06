dbb675ee0b6ee7051f10e22000b63cd85433e43b
Vinayak Sinha <vinayaksinha@zoho.com>
2026-07-06 23:23:23 +0530

    Added top activity places analysis

--- server/app/services/data_analysis.py
+++ server/app/services/data_analysis.py
@@ -1,20 +1,19 @@
 import pandas as pd
 import json
 import calendar
+from location import get_location
 
 with open('../../tests/dump.json', 'r') as file:
     data = json.load(file)
 
 df = pd.DataFrame(data)
-#print(df.iloc[0])
 
+# print(df.iloc[0])
 # walk_km = df.loc[df["type"] == "Walk", "distance"].sum()/1000
-# run_km = df.loc[df["type"] == "Run", "distance"].sum()/1000
-# print(walk_km, run_km)
 
 # Top sports based on distance
-# top_sports = (df.groupby("type")["distance"].sum()/1000).sort_values(ascending=False)
-# print(type_km)
+top_sports = (df.groupby("type")["distance"].sum()/1000).sort_values(ascending=False)
+# print(top_sports)
 
 
 # Total days active, by months
@@ -25,8 +24,7 @@ df = pd.DataFrame(data)
 
 df['start_date'] = pd.to_datetime(df['start_date'], format='%Y-%m-%dT%H:%M:%SZ').dt.normalize()
 df['month'] = df['start_date'].dt.month_name()
-
-# total_days = df['start_date'].nunique()
+total_days = df['start_date'].nunique()
 
 # For duplicate days
 # duplicate_dates = df[df['start_date'].duplicated(keep=False)] #.count() #.nunique()
@@ -34,24 +32,27 @@ df['month'] = df['start_date'].dt.month_name()
 
 # Can also use df.drop_duplicates(subset='start_date') below
 unique_date_rows =  df[~df['start_date'].duplicated(keep='first')]  
-# days_by_months = unique_date_rows['month'].value_counts() 
+days_by_months = unique_date_rows['month'].value_counts() 
 month_names = list(calendar.month_name)[1:] # Remove first index as its an empty string
-# days_by_months = days_by_months.reindex(month_names, fill_value=0).sort_values(ascending=False)
+days_by_months = days_by_months.reindex(month_names, fill_value=0).sort_values(ascending=False)
 # print(days_by_months)
 
 
 # Total time spent, time spent by months
-#total_time = df['elapsed_time'].sum()/3600 # secs to hours
+total_time = df['elapsed_time'].sum()/3600 # secs to hours
 #print(total_time)
-#time_by_months = df.groupby('month')['elapsed_time'].sum()/3600
-#time_by_months = time_by_months.reindex(month_names, fill_value=0).sort_values(ascending=False)
+time_by_months = df.groupby('month')['elapsed_time'].sum()/3600
+time_by_months = time_by_months.reindex(month_names, fill_value=0).sort_values(ascending=False)
 #print(time_by_months)
 
 # Longest streak in days
 consecutive_dates = unique_date_rows['start_date'].sort_values().reset_index(drop=True)
-
 day_difference = consecutive_dates - pd.to_timedelta(consecutive_dates.index, unit='D')
-
-streak_lengths = day_difference.value_counts()  #.groupby(day_difference).transform('size')
-
+streak_lengths = day_difference.value_counts()  
 max_streak = streak_lengths.max()
+
+# Top locations, will have to use get_location function.
+df['place'] = df['start_latlng'].apply(get_location)
+df = df.dropna(subset='place')
+top_places = df['place'].value_counts()
+print(top_places)
--- server/app/services/location.py
+++ server/app/services/location.py
@@ -5,7 +5,9 @@ Sample get function response:
 {'country_code': 'GB', 'city': 'Thamesmead', 'latitude': 51.50372, 'longitude': 0.11982, 'population': 31824, 'state': 'England', 'county': 'Greater London', 'country': 'United Kingdom'}
 '''
 
-def get_location(coords: tuple) -> int:
+def get_location(coords: list) -> int:
+    if len(coords) < 2:
+        return None
     location_details = get(coords)
     location = f"{location_details['city']}, {location_details['state']}"
     return location
